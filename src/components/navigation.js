d08cb1bc86405aba5528284d2d833b2c1d74372c
Manuel <manuel@marginleft.at>
2022-04-04 23:10:03 +0200

    first commit

---
+++ src/components/navigation.js
@@ -0,0 +1,10 @@
+export function MaumaNav(props) {
+    return(
+        <nav class="px-8 pt-8">
+            <div class="bg-black rounded-full flex justify-between text-white px-7 py-5">
+                <span>Mauma</span>
+                <span>i</span>
+            </div>            
+        </nav>
+    )
+}
